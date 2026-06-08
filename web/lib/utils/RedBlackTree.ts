5df06e219e9828c731e16839b5842b4b66214b8f
andrehsb <andrehsb06@gmail.com>
2026-06-08 10:44:49 -0300

    feat: Implementa travessia invertida na arvore redblack

---
+++ web/lib/utils/RedBlackTree.ts
@@ -0,0 +1,45 @@
+import { ReportResponse } from "../api";
+
+export class TreeNode {
+    key: number;
+    data: ReportResponse;
+    left: TreeNode | null = null;
+    right: TreeNode | null = null;
+    color: 'RED' | 'BLACK' = 'RED';
+
+    constructor(key: number, data: ReportResponse) {
+        this.key = key;
+        this.data = data;
+    }
+}
+
+export class RedBlackTree {
+    root: TreeNode | null = null;
+
+    public insert(key: number, data: ReportResponse) {
+        // gigi
+    }
+    public getLatest(limit: number): ReportResponse[] {
+        const result: ReportResponse[] = [];
+
+        const reverseInOrder = (node: TreeNode | null) => {
+            //condição de parada de recursão
+            if (node === null || result.length >= limit) return;
+
+            //vai o mais para a esquerda possível (mais recentes)
+            reverseInOrder(node.left);
+
+            // visita a Raiz e insere no array manipulando o índice
+            if (result.length < limit) {
+                result[result.length] = node.data;
+            }
+
+            //anda para a direita (mais antigos)
+            if (result.length < limit) {
+                reverseInOrder(node.right);
+            }
+        };
+        reverseInOrder(this.root);
+        return result;
+    }
+}
