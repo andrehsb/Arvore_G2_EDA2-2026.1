e95aad89ab90cbb14c06d50e3a08f8a96eb02db9
andrehsb <andrehsb06@gmail.com>
2026-06-08 11:37:52 -0300

    fix: Ordem da travessia

--- web/lib/utils/RedBlackTree.ts
+++ web/lib/utils/RedBlackTree.ts
@@ -21,22 +21,21 @@ export class RedBlackTree {
     }
     public getLatest(limit: number): ReportResponse[] {
         const result: ReportResponse[] = [];
-
         const reverseInOrder = (node: TreeNode | null) => {
             //condição de parada de recursão
             if (node === null || result.length >= limit) return;
 
-            //vai o mais para a esquerda possível (mais recentes)
-            reverseInOrder(node.left);
+            //vai o mais para a direita possível (mais recentes)
+            reverseInOrder(node.right);
 
             // visita a Raiz e insere no array manipulando o índice
             if (result.length < limit) {
                 result[result.length] = node.data;
             }
 
-            //anda para a direita (mais antigos)
+            //anda para a esquerda (mais antigos)
             if (result.length < limit) {
-                reverseInOrder(node.right);
+                reverseInOrder(node.left);
             }
         };
         reverseInOrder(this.root);
