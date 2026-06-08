0b6374bb124968810f9263ce10e70c7dff2b53f6
giovannafg <giovannafelp@gmail.com>
2026-06-08 17:16:28 -0300

    Add inserção básica no red-black tree

--- web/lib/utils/RedBlackTree.ts
+++ web/lib/utils/RedBlackTree.ts
@@ -5,19 +5,47 @@ export class TreeNode {
     data: ReportResponse;
     left: TreeNode | null = null;
     right: TreeNode | null = null;
+    parent: TreeNode | null = null;
     color: 'RED' | 'BLACK' = 'RED';
 
     constructor(key: number, data: ReportResponse) {
         this.key = key;
         this.data = data;
-    }
+    } 
 }
 
 export class RedBlackTree {
     root: TreeNode | null = null;
 
     public insert(key: number, data: ReportResponse) {
-        // gigi
+        const newNode = new TreeNode(key, data);
+
+        if (this.root === null) {
+            newNode.color = 'BLACK';
+            this.root = newNode;
+            return;
+        }
+
+        let current: TreeNode | null = this.root;
+        let parent: TreeNode | null = null;
+
+        while (current !== null) {
+            parent = current;
+
+            if (key < current.key) {
+                current = current.left;
+            } else {
+                current = current.right;
+            }
+        }
+
+        newNode.parent = parent;
+
+        if (parent !== null && key < parent.key) {
+            parent.left = newNode;
+        } else if (parent !== null) {
+            parent.right = newNode;
+        }
     }
     public getLatest(limit: number): ReportResponse[] {
         const result: ReportResponse[] = [];
